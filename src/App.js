31d2738d41d8308db75d96cc999fb7e5ae344533
brittanieb <brittanie.boyko@gmail.com>
2020-08-04 16:29:47 -0400

    imported react-bootstrap's cdn

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import CurrentForecast from './Components/CurrentForecast/CurrentForecast';
 import SearchBar from './Components/SearchBar/SearchBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
