2e91f0a0608344dd9aba4fa988bbb1ddbdb028ae
brittanieb <brittanie.boyko@gmail.com>
2020-08-04 16:17:43 -0400

    created hardcoded currentForecast component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Components/Header/Header';
+import CurrentForecast from './Components/CurrentForecast/CurrentForecast';
 import './App.css';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <CurrentForecast />
+    </>
   );
 }
 
---
+++ src/Components/CurrentForecast/CurrentForecast.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CurrentForecast = () => {
+    return (
+        <div>
+            81 degrees
+        </div>
+    )
+};
+
+export default CurrentForecast;
