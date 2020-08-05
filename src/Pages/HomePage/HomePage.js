b86407972a38ec8bf5565f38fe6f40ac68ab97e8
brittanieb <brittanie.boyko@gmail.com>
2020-08-05 18:18:07 -0400

    make api call to openweatherapi and log response to console

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -2,8 +2,13 @@ import React, { Component } from 'react';
 import Header from '../../Components/Header/Header'
 import CurrentForecast from '../../Components/CurrentForecast/CurrentForecast';
 import SearchBar from '../../Components/SearchBar/SearchBar';
+import axios from 'axios';
 
 class HomePage extends Component {
+    state = {
+        latitude: '',
+        longitude: ''
+      }
 
     getUserLocation = () => {
         if (navigator.geolocation) {
@@ -14,12 +19,29 @@ class HomePage extends Component {
     }
 
     showPosition = (position) => {
-        console.log("latitude:", position.coords.latitude, "longitude:", position.coords.longitude)
+        this.setState({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+        this.getWeather()
+
     }
 
     componentDidMount() {
         this.getUserLocation();
     }
+
+    getWeather() {
+        const openWeatherAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.latitude}&lon=${this.state.longitude}&
+        exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`
+        axios.get(openWeatherAPI)
+        .then(res => {
+            console.log(res.data);
+        }).catch(err => {
+            console.log(err);
+        }) 
+    }
+
     render() {
         return (
           <>
@@ -33,15 +55,3 @@ class HomePage extends Component {
 }
 
 export default HomePage;
-
-
-// axios({
-//     method: 'get',
-//     url: 'http://bit.ly/2mTM3nY',
-//     responseType: 'stream'
-//   })
-//     .then(function (response) {
-//       response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'))
-//     });
-
-// process.env.REACT_APP_GOOGLE_API_KEY
