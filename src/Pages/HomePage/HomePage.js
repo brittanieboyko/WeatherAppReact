09e97e067ea57a40d2058de11c9f56d7c599db79
brittanieb <brittanie.boyko@gmail.com>
2020-08-08 19:59:19 -0400

    added real date using moment

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -3,6 +3,7 @@ import Header from "../../Components/Header/Header";
 import CurrentForecast from "../../Components/CurrentForecast/CurrentForecast";
 import SearchBar from "../../Components/SearchBar/SearchBar";
 import DailyForecastContainer from "../../Components/DailyForecastContainer/DailyForecastContainer";
+import Moment from 'react-moment';
 import axios from "axios";
 
 class HomePage extends Component {
@@ -79,9 +80,11 @@ class HomePage extends Component {
   }
 
   render() {
+    const dateToFormat = new Date();
     return (
       <>
         <Header />
+        <Moment format="dddd, MMM D">{dateToFormat}</Moment>
         <CurrentForecast temperature={this.state.temperature} />
         <SearchBar />
         <DailyForecastContainer sevenDayForecast={this.state.sevenDayForecast}/>
