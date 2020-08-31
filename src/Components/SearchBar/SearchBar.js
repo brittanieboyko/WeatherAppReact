fcdc784e901fb62dede46d6478708bfa45b49de4
brittanieb <brittanie.boyko@gmail.com>
2020-08-31 18:12:59 -0400

    fixed formatting

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,23 +1,25 @@
-import React from 'react';
-import InputGroup from 'react-bootstrap/InputGroup';
-import FormControl from 'react-bootstrap/FormControl';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 const SearchBar = (props) => {
-    return (
-        <>
-            <InputGroup className="mb-3">
-                <FormControl
-                placeholder="Enter a location"
-                aria-label="Search a location"
-                onChange={props.onChange}
-                />
-                <InputGroup.Append>
-                    <Button variant="outline-secondary" onClick={props.onSubmit}>Search</Button>
-                </InputGroup.Append>
-            </InputGroup>
-        </>
-    )
+  return (
+    <>
+      <InputGroup className="mb-3">
+        <FormControl
+          placeholder="Enter a location"
+          aria-label="Search a location"
+          onChange={props.onChange}
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary" onClick={props.onSubmit}>
+            Search
+          </Button>
+        </InputGroup.Append>
+      </InputGroup>
+    </>
+  );
 };
 
 export default SearchBar;
