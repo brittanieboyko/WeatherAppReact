3116a3076589303aa6e9ef5e242f6b7f6e4f36c4
brittanieb <brittanie.boyko@gmail.com>
2020-09-07 19:28:54 -0400

    only render in + city name in header when weather is provided

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Moment from 'react-moment';
+import React from "react";
+import Moment from "react-moment";
 import Container from "react-bootstrap/Container";
 
 const Header = (props) => {
-    const dateToFormat = new Date();
-    return (
-        <Container>
-            <h1> Weather {props.cityName}</h1>
-            <Moment format="dddd, MMM D">{dateToFormat}</Moment>
-        </Container>
-    )
+  const dateToFormat = new Date();
+  return (
+    <Container>
+      {props.cityName ? (
+        <h1> Weather in {props.cityName}</h1>
+      ) : (
+        <h1>Weather</h1>
+      )}
+      <Moment format="dddd, MMM D">{dateToFormat}</Moment>
+    </Container>
+  );
 };
 
 export default Header;
