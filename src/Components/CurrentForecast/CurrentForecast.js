b17eb37ddf4f87f023be354b368ac13542ce93f6
brittanieb <brittanie.boyko@gmail.com>
2020-09-07 19:32:07 -0400

    only render current temp once weather api data is provided

--- src/Components/CurrentForecast/CurrentForecast.js
+++ src/Components/CurrentForecast/CurrentForecast.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 import Container from "react-bootstrap/Container";
 
 const CurrentForecast = (props) => {
-    return (
-        <Container>
-            {props.temperature} degrees
-        </Container>
-    )
+  return (
+    <Container>
+      {props.temperature ? <h2>Curently {props.temperature} degrees </h2> : <h2> </h2>}
+    </Container>
+  );
 };
 
 export default CurrentForecast;
