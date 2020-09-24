c80014e04dc0b3da6a4ad3fa5df61ff02040d6a5
brittanieb <brittanie.boyko@gmail.com>
2020-09-23 22:16:13 -0400

    centered text in header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import CurrentForecast from "../../Components/CurrentForecast/CurrentForecast";
+import "./Header.css";
 
 const Header = (props) => {
   return (
     <Jumbotron>
       {props.cityName ? (
-        <>
+        <div>
           <h1> Weather in {props.cityName}</h1>
           <CurrentForecast temperature={props.temperature} />
-        </>
+        </div>
       ) : (
         <h1>Weather</h1>
       )}
