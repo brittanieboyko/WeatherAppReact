7f0eafddbbfe0c56f24bfce2256ae482925cef73
brittanieb <brittanie.boyko@gmail.com>
2020-09-23 22:24:21 -0400

    removed location button from search bar component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,27 +3,10 @@ import InputGroup from "react-bootstrap/InputGroup";
 import FormControl from "react-bootstrap/FormControl";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Spinner from "react-bootstrap/Spinner";
 
 const SearchBar = (props) => {
   return (
     <Container>
-      {props.loading ? (
-        <Button variant="outline-secondary" disabled>
-          <Spinner
-            as="span"
-            animation="grow"
-            size="sm"
-            role="status"
-            aria-hidden="true"
-          />
-          Loading...
-        </Button>
-      ) : (
-        <Button variant="outline-secondary" onClick={props.onClick}>
-          Use My Current Location
-        </Button>
-      )}
       <InputGroup className="mb-3">
         <FormControl
           placeholder="Enter a city"
