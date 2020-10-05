1e82b704e2125489d4e377b4d0c0dcdd6bf1afe2
brittanieb <brittanie.boyko@gmail.com>
2020-10-05 18:21:17 -0400

    added margin spacing between forecast cards

--- src/Components/DailyForecast/DailyForecast.js
+++ src/Components/DailyForecast/DailyForecast.js
@@ -9,24 +9,26 @@ const DailyForecast = (props) => {
 
   return (
     <Col sm={4}>
-      <Card>
-        <Card.Img
-          variant="top"
-          src={`http://openweathermap.org/img/wn/${props.value.weather[0].icon}@2x.png`}
-          className="icon"
-        />
-        <Card.Body>
-          <Card.Title>
-            <Moment add={{ days: props.dayOfWeek }} format="dddd">
-              {todaysDate}
-            </Moment>
-          </Card.Title>
-          <Card.Text>
-            {props.value.temp.max.toFixed(0)} &deg;
-          </Card.Text>
-          <Card.Text>{props.value.weather[0].description}</Card.Text>
-        </Card.Body>
-      </Card>
+      <div className="weather-card">
+        <Card>
+          <Card.Img
+            variant="top"
+            src={`http://openweathermap.org/img/wn/${props.value.weather[0].icon}@2x.png`}
+            className="icon"
+          />
+          <Card.Body>
+            <Card.Title>
+              <Moment add={{ days: props.dayOfWeek }} format="dddd">
+                {todaysDate}
+              </Moment>
+            </Card.Title>
+            <Card.Text>
+              {props.value.temp.max.toFixed(0)} &deg;
+            </Card.Text>
+            <Card.Text>{props.value.weather[0].description}</Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
     </Col>
   );
 };
