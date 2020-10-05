042208ef5e2db7fb9544bc9ffbd5b2af2c539609
brittanieb <brittanie.boyko@gmail.com>
2020-10-05 18:14:09 -0400

    put dailyforecast rows into bootstrap container

--- src/Components/DailyForecastContainer/DailyForecastContainer.js
+++ src/Components/DailyForecastContainer/DailyForecastContainer.js
@@ -1,6 +1,7 @@
 import React from "react";
 import DailyForecast from "../DailyForecast/DailyForecast";
 import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
 import "./DailyForecastContainer.css";
 
 const DailyForecastContainer = (props) => {
@@ -8,19 +9,21 @@ const DailyForecastContainer = (props) => {
 
   return (
     <div className="forecast-container">
-      <Row>
-        {dailyForecasts ? (
-          dailyForecasts.map((forecast) => (
-            <DailyForecast
-              key={Math.random() * 10}
-              value={forecast}
-              dayOfWeek={dailyForecasts.indexOf(forecast)}
-            />
-          ))
-        ) : (
-          <p></p>
-        )}
-      </Row>
+      <Container>
+        <Row>
+          {dailyForecasts ? (
+            dailyForecasts.map((forecast) => (
+              <DailyForecast
+                key={Math.random() * 10}
+                value={forecast}
+                dayOfWeek={dailyForecasts.indexOf(forecast)}
+              />
+            ))
+          ) : (
+            <p></p>
+          )}
+        </Row>
+      </Container>
     </div>
   );
 };
